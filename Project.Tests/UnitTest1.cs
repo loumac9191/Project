261fa109246f79b7a03b0252e9a3f0b2a9bae23f
loumac9191 <loumac91@hotmail.co.k>
2016-02-29 12:17:51 +0000

    blank assembly

---
+++ Project.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Project.NET;
+
+namespace Project.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
