e14c3587030c31fcf387e9b85fddc7cb86c598b6
loumac9191 <loumac91@hotmail.co.k>
2016-02-29 13:53:44 +0000

    adding code logging

--- ECommerce.Project/Program.cs
+++ ECommerce.Project/Program.cs
@@ -1,15 +1,30 @@
-﻿using System;
+﻿using log4net;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
+
 namespace ECommerce.Project
 {
     class Program
     {
+        private static readonly ILog logger = LogManager.GetLogger("Program.cs");
+
         static void Main(string[] args)
         {
+            try
+            {
+                int number1 = 10; 
+                int number2 = 0;
+                int result = number1 / number2;
+            }
+            catch (DivideByZeroException exception)
+            {
+                logger.Error(exception.Message); Console.WriteLine("You can't divide by zero!");
+            }
         }
     }
 }
