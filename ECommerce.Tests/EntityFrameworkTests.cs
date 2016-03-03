cbb923209cb4531290e72cff1682247da63bcd28
loumac9191 <loumac91@hotmail.co.k>
2016-03-03 12:19:07 +0000

    All 7 first tests pass

--- ECommerce.Tests/EntityFrameworkTests.cs
+++ ECommerce.Tests/EntityFrameworkTests.cs
@@ -9,21 +9,27 @@ namespace ECommerce.Tests
     [TestClass]
     public class EntityFrameworkTests
     {
-        //[TestMethod]
-        //public void TestBasketAddItem_CallsIItemRepositoryAddItem_WhenGivenAnItemToAdd()
-        //{
-        //    //Arrange
-        //    Mock<ItemRepository> mockIRepos = new Mock<ItemRepository>();
-        //    Mock<item> mItem = new Mock<item>();
-        //    mItem.Object.name = "adidas Mens Brazuca Top Replique Ball";
-        //    Basket basket = new Basket(mockIRepos.Object);
+        [TestMethod]
+        public void TestBasketAddItem_CallsIItemRepositoryAddItem_WhenGivenAnItemToAdd()
+        {
+            //Arrange
+            //Mock<ItemRepository> mockIRepos = new Mock<ItemRepository>();
+            //Mock<item> mItem = new Mock<item>();
+            //mItem.Object.name = "adidas Mens Brazuca Top Replique Ball";
+            //Basket basket = new Basket(mockIRepos.Object);
 
-        //    //Act
-        //    basket.AddItem(mItem.Object.name);
+            string itemName = "adidas Mens Brazuca Top Replique Ball";
+            Mock<ItemRepository> iRepository = new Mock<ItemRepository>();
+            item returnedItem = new item() { name = itemName, item_id = 1 };
+            Basket tBasket = new Basket(iRepository.Object);
 
-        //    //Assert
-        //    mockIRepos.Verify(r => r.RetrieveItemByName(mItem.Object.name));
-        //}
+            iRepository.Setup(x => x.RetrieveItemByName(It.Is<string>(s => s == itemName))).Returns(returnedItem);
+            //Act
+            tBasket.AddItem(itemName);
+
+            //Assert
+            iRepository.Verify(r => r.RetrieveItemByName(returnedItem.name));
+        }
         //[TestMethod]
         //public void TestBasketRemoveItem_CallsIItemRepositoryRemoveItem_WhenGivenAnItemToRemove()
         //{
