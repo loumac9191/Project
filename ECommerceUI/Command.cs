b35f5b3319eb3570523b7ed5b60440b4c3009e05
loumac9191 <loumac91@hotmail.co.k>
2016-03-08 17:24:51 +0000

    UI first implementation

---
+++ ECommerceUI/Command.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ECommerceUI
+{
+    public class Command : ICommand
+    {
+        private readonly Action _execute;
+        private readonly Func<bool> _canExecute;
+        public event EventHandler CanExecuteChanged; //event handler - going to update the UI whenever the canexecute changes
+
+        public Command(Action execute, Func<bool> canExecute = null)
+        {
+            if (execute == null)
+            {
+                throw new ArgumentNullException("execute");
+            }
+            _canExecute = canExecute ?? (() => true); //lambda expression that doesnt have an input, question marks are to do with if the first side is true then do that else do the right side
+            _execute = execute;
+        }
+
+        [DebuggerStepThrough]
+        public bool CanExecute(object parameter)
+        {
+            try
+            {
+                return _canExecute();
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public void Execute(object parameter)
+        {
+            if (!CanExecute(parameter))
+            {
+                return;
+            }
+            try
+            {
+                _execute();
+            }
+            catch
+            {
+                Debugger.Break(); //if you get any exception when runningg this then just crash
+            }
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
---
+++ ECommerceUI/ViewModels/BaseViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceUI.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName) 
+        {        
+            if (PropertyChanged != null)
+            {
+                //this refers to the class that is firing the event(i.e. it is THIS class)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
