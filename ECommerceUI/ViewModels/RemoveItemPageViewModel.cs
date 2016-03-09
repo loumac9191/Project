b7216885bc037122a8a37342870fa257fc1e342f
loumac9191 <loumac91@hotmail.co.k>
2016-03-09 12:13:18 +0000

    navigatebetweenOptions

---
+++ ECommerceUI/ViewModels/RemoveItemPageViewModel.cs
@@ -0,0 +1,104 @@
+﻿using ECommerce.Project;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ECommerceUI.ViewModels
+{
+    public class RemoveItemPageViewModel
+    {
+        private ICommand _searchFor;
+
+        public ICommand searchFor
+        {
+            get
+            {
+                if (_searchFor == null)
+                {
+                    _searchFor = new Command(SearchFor, CanSearchFor);
+                }
+                return _searchFor;
+            }
+            set { _searchFor = value; }
+        }
+
+        private ICommand _addItem;
+
+        public ICommand addItem
+        {
+            get
+            {
+                if (_addItem == null)
+                {
+                    _addItem = new Command(AddItem, CanAddItem);
+                }
+                return _addItem;
+            }
+            set { _addItem = value; }
+        }
+
+        private ICommand _removeItem;
+
+        public ICommand removeItem
+        {
+            get
+            {
+                if (_removeItem == null)
+                {
+                    _removeItem = new Command(RemoveItem, CanRemoveItem);
+                }
+                return _removeItem;
+            }
+            set { _removeItem = value; }
+        }
+
+                public void SearchFor()
+        {
+            MainViewModel vm = App.Current.MainWindow.DataContext as MainViewModel;
+            vm.page = "SearchItemPage.xaml";
+        }
+
+        public bool CanSearchFor()
+        {
+            return true;
+        }
+
+        public void AddItem()
+        {
+            MainViewModel vm = App.Current.MainWindow.DataContext as MainViewModel;
+            vm.page = "AddStockPage.xaml";
+        }
+
+        public bool CanAddItem()
+        {
+            return true;
+        }
+
+        public void RemoveItem()
+        {
+
+        }
+
+        public bool CanRemoveItem()
+        {
+            return true;
+        }
+
+        //private void UpdateCommands()
+        //{
+        //    ((Command)registerDetails).RaiseCanExecuteChanged();
+        //}
+
+        private LoginController _loginController;
+
+        public RemoveItemPageViewModel()
+        {
+            _loginController = new LoginController();
+        }
+
+
+    }
+}
